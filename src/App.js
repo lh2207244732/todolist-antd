b49113496fabf64f7ab619cd640be0375f6d728a
bright <2207244732.com>
2020-11-23 17:59:26 +0800

    add List

--- src/App.js
+++ src/App.js
@@ -1,69 +1,72 @@
 import React from 'react'
 import axios from 'axios'
 import regeneratorRuntime from "regenerator-runtime";
-import {  DatePicker } from 'antd';
-import { Input } from 'antd';
+import { Input, Button, Row, Col, List } from 'antd';
 
-import Item from './item'
+export default class App extends React.Component {
 
-export default class App extends React.Component{
-
-    constructor(props){
+    constructor(props) {
         super(props)
-        this.state={
-            list:[],
-            task:''
+        this.state = {
+            list: [],
+            task: ''
         }
-        this.handleChange=this.handleChange.bind(this)
-        this.handleAdd=this.handleAdd.bind(this)
-        this.deleteItem=this.deleteItem.bind(this)
+        this.handleChange = this.handleChange.bind(this)
+        this.handleAdd = this.handleAdd.bind(this)
+
     }
-    
-    handleChange(event){
-        const task=event.target.value
+
+    handleChange(event) {
+        const task = event.target.value
         this.setState({
             task
         })
     }
-    handleAdd(){
-        const {task}=this.state
-        const item={
-            task:task,
-            id:Date.now()
+    handleAdd() {
+        const { task } = this.state
+        const item = {
+            task: task,
+            id: Date.now()
         }
-        const list=[...this.state.list]
+        const list = [...this.state.list]
         list.push(item)
         this.setState({
             list,
-            task:''
+            task: ''
         })
     }
-    deleteItem(id){
-        let {list}=this.state
-        list=list.filter(item=>item.id!=id)
+    deleteItem(id) {
+        let { list } = this.state
+        list = list.filter(item => item.id != id)
         this.setState({
             list
         })
     }
 
-    async componentDidMount(){
-        const result=await axios.get('http://127.0.0.1:3000')
+    async componentDidMount() {
+        const result = await axios.get('http://127.0.0.1:3000')
         this.setState({
-            list:result.data
+            list: result.data
         })
     }
 
-    render(){
-        const {task,list}=this.state
+    render() {
+        const { task, list } = this.state
         return (
-            <div className ='app'>
-                <div className='head'>
-                {/* <input value={task} onChange={this.handleChange} /> */}
-                <Input placeholder="请输入..." value={task} onChange={this.handleChange} />
-             <button onClick={this.handleAdd}>提交</button>
-            </div >
-            <Item list={list} deleteItem={this.deleteItem} />
-            </div> 
+            <div className='app'>
+                <Row>
+                    <Col span={16}>  <Input placeholder="请输入..." value={task} onChange={this.handleChange} /></Col>
+                    <Col span={8}><Button type="primary" onClick={this.handleAdd}>提交</Button></Col>
+                </Row>
+                <List
+                    dataSource={list}
+                    renderItem={item => (
+                        <List.Item onClick={this.deleteItem.bind(this, item.id)}>
+                            {item.task}
+                        </List.Item>
+                    )}
+                />
+            </div>
         )
     }
 }
